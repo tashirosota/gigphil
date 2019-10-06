17c2bbfd7c43b99e344a35db060cdbeef50c1c09
Tashiro <g4160hc@gmail.com>
2019-10-06 22:10:36 +0900

    クローリングもんちょい

--- app/crawlers/base_crawler.rb
+++ app/crawlers/base_crawler.rb
@@ -4,9 +4,10 @@
 class BaseCrawler
   class << self 
     def execute!
-      new_schedule, exist_schedule = divide_by_new_and_existing crawlering
-      update_schedules! new_schedule
-      insert_schedules! new_schedule!
+      schedules = crawlering 
+      new_schedule, exist_schedule = divide_by_new_and_existing schedules
+      update_schedules! schedules.name exist_schedule
+      insert_schedules! schedules.name, new_schedule
     end
   
     private
@@ -19,12 +20,43 @@ class BaseCrawler
       raise 'Must implement #crawlering'
     end
   
-    def update_schedules!(schedules)
-      
+    def update_schedules!(name, schedules)
+      return unless hako = MusicBar.find_by(title: name)
+      schedules.each do |schedule|
+        hako.schedules.find(date: schedule.date).update schedule
+      end
     end
   
-    def insert_schedules!(schedules)
-      
+    def insert_schedules!(name, schedules)
+      return unless hako = MusicBar.find_by(title: name)
+      hako.schedule.create_all! schedules
     end
   end
 end
+
+# crawleringの返り値(OpenStruct)
+# {
+#   name: '大塚Deepa',
+#   schedules: [
+#     {
+#       title: String,
+#       date: String,
+#       adv: String?,
+#       door: String?,
+#       open: Time?,
+#       start: Time?,
+#       act: [String, String],
+#       info: String?
+#     },
+#     {
+#       title: String,
+#       date: String,
+#       adv: String?,
+#       door: String?,
+#       open: Time?,
+#       start: Time?,
+#       act: [String, String],
+#       info: String?
+#     }
+#   ]
+# }
