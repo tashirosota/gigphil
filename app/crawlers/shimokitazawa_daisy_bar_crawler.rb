9087669437f41ae55f3e19bfc85b23b4ea33b62f
Tashiro <g4160hc@gmail.com>
2019-10-14 22:20:28 +0900

    頑張る

---
+++ app/crawlers/shimokitazawa_daisy_bar_crawler.rb
@@ -0,0 +1,24 @@
+class OtsukaDeepaCrawler < BaseCrawler
+  CALENDAR_PATH = '/events/event/on'
+
+  def initialize(term)
+    super term
+    @bar = MusicBar.find_by!(name: '下北沢DaisyBar')
+  end
+
+  def execute!
+    @term.times do |i|
+      month = (now.month + i).to_s.rjust(2, '0')
+      request_url = @bar.hp + CALENDAR_PATH + '/'  + current_year + '/' + month
+      save_crawling_result(url: request_url, parser: nokogiri) do |doc|
+        format(doc: doc)
+      end
+    end
+  end
+
+  private
+
+  def format(doc:)
+    # TODO
+  end
+end
