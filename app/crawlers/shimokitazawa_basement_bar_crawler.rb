5db0b4443b67a599a14ac224ac485cc91a2464cd
Tashiro <g4160hc@gmail.com>
2019-10-18 23:21:29 +0900

    rubocop

--- app/crawlers/shimokitazawa_basement_bar_crawler.rb
+++ app/crawlers/shimokitazawa_basement_bar_crawler.rb
@@ -6,30 +6,36 @@ class ShimokitazawaBasementBarCrawler < BaseCrawler
     @bar = MusicBar.find_by!(name: '下北沢BASEMENTBAR')
   end
 
+  # rubocop:disable Metrics/AbcSize
   def execute!
     @term.times do |i|
       daily_ary(i).each do |day|
         month = (now.month + i).to_s.rjust(2, '0')
         request_url = @bar.hp + CALENDAR_PATH + '/' + current_year_str + '/' + month + '/' + day
-        pp request_url
         save_crawling_result(url: request_url, parser: nokogiri) do |doc|
-          format(date: Date.new(current_year_str.to_i,  current_month_str.to_i + i, day.to_i), doc: doc, )
+          format(
+            date: Date.new(current_year_str.to_i, current_month_str.to_i + i, day.to_i),
+            doc: doc
+          )
         end
       end
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   private
 
   def format(date:, doc:)
     doc.css('.content-area article').each_with_object([]) do |li_element, events|
-      title = li_element.css('.eo-event-title.entry-title').text.gsub(/[\r\n\t]/,"")
+      title = li_element.css('.eo-event-title.entry-title').text.gsub(/[\r\n\t]/, '')
       next if title.blank? || title.include?('OFF') || title.include?('メンテナンス')
+
       info, artists_str = li_element.css('.eo-event-content').text.split(/ACT/).map(&:strip)
       event = OpenStruct.new(
         title: title,
         date: date,
-        act: (artists_str&.split(/ /) || [])+ [artists_str], # basementbarはアーティスト名入力してないケースが多すぎる フライヤー表示も考えないとイケナイ
+        # basementbarはアーティスト名入力してないケースが多すぎる フライヤー表示も考えないとイケナイ
+        act: (artists_str&.split(/ /) || []) + [artists_str],
         info: info
       )
       events << event
@@ -38,6 +44,6 @@ class ShimokitazawaBasementBarCrawler < BaseCrawler
 
   def daily_ary(term_count)
     day_count = Date.new(current_year_str.to_i,  current_month_str.to_i + term_count, -1).day
-    (1..day_count).map { |day| day.to_s.rjust(2, '0') } 
+    (1..day_count).map { |day| day.to_s.rjust(2, '0') }
   end
 end
