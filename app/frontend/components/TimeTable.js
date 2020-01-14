280368e7bcc78a79e39fa7e79d61282b3eda2ba4
Tashiro <g4160hc@gmail.com>
2020-01-15 08:59:42 +0900

    layout設計ざっくり全体

--- app/frontend/components/TimeTable.js
+++ app/frontend/components/TimeTable.js
@@ -2,12 +2,12 @@ import React from "react"
 import styled from 'styled-components'
 import swal from 'sweetalert';
 
-const defalutRecord = {
-  order: 1,
+var defalutRecord = {
+  order: '',
   bandName: 'バンド名',
   customPlayTime: null,
   customSettingTime: null,
-  memo: null
+  memo: '備考'
 }
 export default class TimeTable extends React.Component {
 
@@ -17,14 +17,15 @@ export default class TimeTable extends React.Component {
       eventDate: null,
       title: null,
       place: null,
+      memo: null,
       meetingTime: null,
       openTime: null,
       rehearsalSettingTime: null,
       rehearsalPlayTime: null,
       productionSettingTime: null,
       productionPlayTime: null,
-      rehearsals: defaultRecords(), 
-      concerts: defaultRecords()
+      rehearsals: this.defaultRecords(), 
+      concerts: this.defaultRecords()
     }
   }
 
@@ -41,21 +42,203 @@ export default class TimeTable extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <Title>{this.state.title}</Title>
-        <EventDate>{this.state.eventDate}</EventDate>
-        <Place>{this.state.place}</Place>
+        <Container>
+          <Title>
+            <Input placeholder='バンド名'>{this.state.title}</Input>
+          </Title>
+          <EventDate>
+            <Input placeholder='日付'>{this.state.eventDate}</Input>
+          </EventDate>          
+          <Place>
+            <Input placeholder='場所'>{this.state.place}</Input>
+          </Place>
+          <Memo>
+            <TextArea placeholder='備考'>{this.state.memo}</TextArea>
+          </Memo>
+          <Rehearsal>
+            <TimesContainer>
+              <DefaultPlayTime><Input placeholder='順番'/></DefaultPlayTime>
+              <DefaultSettingTime><Input placeholder='順番'/></DefaultSettingTime>
+            </TimesContainer>
+            <Table className='table table-bordered'>
+              <Thead>
+                <Tr>
+                  <Th scope="col">順番</Th>
+                  <Th scope="col">バンド名</Th>
+                  <Th scope="col">時間</Th>
+                  <Th scope="col">演奏</Th>
+                  <Th scope="col">転換</Th>
+                  <Th scope="col">備考</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+              {
+                this.state.concerts.map((record, index) => {
+                  return <Tr key={index}>
+                    <Td><Input placeholder='順番' defaultValue={record.order}/></Td>
+                    <Td><Input placeholder='バンド名' defaultValue={record.bandName}/></Td>
+                    <Td>時間</Td>
+                    <Td><Input placeholder='演奏' defaultValue={record.customPlayTime}/></Td>
+                    <Td><Input placeholder='転換' defaultValue={record.customSettingTime}/></Td>
+                    <Td><Input placeholder='備考' defaultValue={record.memo}/></Td>
+                    <ButtonArea>
+                      <AddButton/>
+                      <DeleteButton/>
+                    </ButtonArea>
+                  </Tr>
+                 }
+                )
+              }
+              </Tbody>
+            </Table>
+          </Rehearsal>
+          <TimesContainer>
+            <Meeting><Input placeholder='顔合わせ'/></Meeting>
+            <Open><Input placeholder='Open'/></Open>
+            <Start><Input placeholder='Start'/></Start>
+          </TimesContainer>
+          <Production>
+            <TimesContainer>
+              <DefaultPlayTime><Input placeholder='順番'/></DefaultPlayTime>
+              <DefaultSettingTime><Input placeholder='順番'/></DefaultSettingTime>
+            </TimesContainer>           
+            <Table className='table table-bordered'>
+             <Thead>
+                <Tr>
+                  <Th scope="col">順番</Th>
+                  <Th scope="col">バンド名</Th>
+                  <Th scope="col">時間</Th>
+                  <Th scope="col">演奏</Th>
+                  <Th scope="col">転換</Th>
+                  <Th scope="col">備考</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {this.state.concerts.map((record, index) => {
+                  return <Tr key={index}>
+                    <Td><Input placeholder='順番' defaultValue={record.order}/></Td>
+                    <Td><Input placeholder='バンド名' defaultValue={record.bandName}/></Td>
+                    <Td>時間</Td>
+                    <Td><Input placeholder='演奏' defaultValue={record.customPlayTime}/></Td>
+                    <Td><Input placeholder='転換' defaultValue={record.customSettingTime}/></Td>
+                    <Td><Input placeholder='備考' defaultValue={record.memo}/></Td>
+                    <ButtonArea>
+                      <AddButton/>
+                      <DeleteButton/>
+                    </ButtonArea>
+                  </Tr>
+                 }
+                )}
+              </Tbody>
+            </Table>
+          </Production>
+          <SaveButtop>出力</SaveButtop>
+        </Container>
       </React.Fragment>
     );
   }
 }
 
-const Title = styled.h1`
+const Container = styled.div`
+  display: block;
+  width: 980px;
+  margin: 0 auto;
+`
+
+const Input = styled.input`
   
 `
 
-const EventDate = styled.h1`
+const TextArea =  styled.textarea`
   
 `
 
-const Place = styled.h1`
+const Title = styled.div`
+  
+`
+
+const EventDate = styled.div`
+  
+`
+
+const Place = styled.div`
+`
+
+const Memo = styled.div`
+  width: 100px;
+`
+const TimesContainer = styled.div`
+  display: flex;
+`
+
+const Meeting = styled.div`
+
+`
+
+const Open = styled.div`
+
+`
+
+const Start = styled.div`
+
+`
+
+const DefaultPlayTime = styled.div`
+
+`
+
+const DefaultSettingTime = styled.div`
+
+`
+
+const Rehearsal = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+`
+
+const Production = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+`
+
+const Table = styled.table`
+
+`
+
+const Thead = styled.thead`
+
+`
+
+const Tr = styled.tr`
+
+`
+
+const Th = styled.th`
+
+`
+
+const Tbody = styled.tbody`
+
+`
+
+const Td = styled.td`
+
+`
+
+const SaveButtop = styled.button`
+  
+`
+
+const ButtonArea = styled.div`
+  
+`
+
+const AddButton = styled.button`
+  
+`
+
+const DeleteButton  = styled.button`
+  
 `
