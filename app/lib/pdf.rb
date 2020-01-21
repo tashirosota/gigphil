39b1462c325f098507837f1534650a310c5a8a37
yodaka <engineeryodaka@gmail.com>
2020-01-21 21:24:18 +0900

    とりあえずtable作成。

--- app/lib/pdf.rb
+++ app/lib/pdf.rb
@@ -15,12 +15,41 @@ class Pdf < Prawn::Document
     font 'app/assets/fonts/k-gothic.ttf'
     stroke_axis
 
-    header
+    details
     move_down 50
-    contents
+    text "持ち時間: 20分　転換時間 5分"
+    move_down 50
+    rehearsals
+    move_down 50
+    text "顔合わせ: 17:30 OPEN:18:00 START:18:30"
+    concerts
 
     # @record = record # インスタンスを受け取り。コンポーネント作成時などにレコード内のデータを使える
-    text 'hello よだか'
+    # text 'hello よだか'
 
+  end
+
+  # @todo中央寄せ
+  def details
+    table([
+              ["タイトル", "short"],
+              ["日付", "loooooooooooooooooooong"],
+              ["場所", "short"],
+              ["備考", "short"]
+          ], :position => :center, :width => 500, :column_widths => {0 => 80})
+  end
+
+  def rehearsals
+    table([
+              ["順番", "バンド名", '時間', '演奏', '転換', '備考'],
+              ["順番", "バンド名", '時間', '演奏', '転換', '備考'],
+          ])
+  end
+
+  def concerts
+    table([
+              ["順番", "バンド名", '時間', '演奏', '転換', '備考'],
+              ["順番", "バンド名", '時間', '演奏', '転換', '備考'],
+          ])
   end
 end
