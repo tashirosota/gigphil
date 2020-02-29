984f7b6c3175413fac3d5870afe4b640d6372e9c
Tashiro <g4160hc@gmail.com>
2020-02-29 20:47:23 +0900

    ログインを促すコンポーネントを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :ensure_domain, :authenticate!, :snakeize_params
-  helper_method :current_user
+  helper_method :current_user, :logined?
   add_flash_types :secondary
 
   private
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logined?
+    !!current_user
   end
 
   def snakeize_params
