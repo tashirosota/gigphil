d5509b2e6dd8afe6bd29472410eb7f52dcaa9176
Tashiro <g4160hc@gmail.com>
2020-02-29 20:17:10 +0900

    tagとareaの初期データ追加

--- app/controllers/indies_wiki/home_controller.rb
+++ app/controllers/indies_wiki/home_controller.rb
@@ -1,64 +1,67 @@
 class IndiesWiki::HomeController < ApplicationController
   skip_before_action :authenticate!
   def show
+    tags = Tag.all.map(&:name)
+    areas = Area.all.map(&:name)
+    artists = RegisteredArtist.all.map(&:name)
     @artists = []
     @artists << {
-      name: 'Someday\'s gone',
-      area: '宇都宮',
+      name: artists.sample,
+      area: areas.sample,
       tags: %w(メロディー ギターロック パワーポップ),
       icon: 'http://kotori-band.com/img/_member/011.jpg'
     }
     @artists << {
-      name: 'KOTORI',
-      area: '宇都宮',
-      tags: %w(メロディー ギターロック パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags: tags.shuffle.take(3),
       icon: 'https://somedaysgone.com/assets/imgs/about/somedaysgone.jpg'
     }
     @artists << {
-      name: 'SonoSheet',
-      area: '宇都宮',
-      tags: %w(反省 メロコア パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags: tags.shuffle.take(3),
       icon: 'https://66.media.tumblr.com/d44fc715131afeddaa139b7aa751c314/tumblr_inline_pk1i2dLF4P1tk8unz_500.jpg'
     }
     @artists << {
-      name: 'soshaku',
-      area: 'せんかわ',
-      tags: %w(おじさん おさけ パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags: tags.shuffle.take(3),
       icon: 'http://soshaku.wp.xdomain.jp/wp-content/themes/soshaku/img/profile.jpg'
     }
     @artists << {
-      name: 'FANCY GIRL CINEMA',
-      area: '大塚',
-      tags: %w(パワーポップ Oasis パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags: tags.shuffle.take(3),
       icon: 'https://static.wixstatic.com/media/28e8f4_ff1d23368e6841c99397a8e75f1f9040~mv2.jpg/v1/fill/w_1544,h_931,al_c,q_85/21469.webp'
     }
     @artists << {
-      name: 'SonoSheet',
-      area: '宇都宮',
-      tags: %w(反省 メロコア パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags:  tags.shuffle.take(3),
       icon: 'https://66.media.tumblr.com/d44fc715131afeddaa139b7aa751c314/tumblr_inline_pk1i2dLF4P1tk8unz_500.jpg'
     }
     @artists << {
-      name: 'soshaku',
-      area: 'せんかわ',
-      tags: %w(おじさん おさけ パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags:  tags.shuffle.take(3),
       icon: 'http://soshaku.wp.xdomain.jp/wp-content/themes/soshaku/img/profile.jpg'
     }
     @artists << {
-      name: 'FANCY GIRL CINEMA',
-      area: '大塚',
-      tags: %w(パワーポップ Oasis パワーポップ),
+      name: artists.sample,
+      area: areas.sample,
+      tags:  tags.shuffle.take(3),
       icon: 'https://static.wixstatic.com/media/28e8f4_ff1d23368e6841c99397a8e75f1f9040~mv2.jpg/v1/fill/w_1544,h_931,al_c,q_85/21469.webp'
     }
 
     @artists << {
-      name: 'fish in water project',
-      area: '高崎',
-      tags: %w(おしゃれさん 電子音 シュッとしてる),
+      name: artists.sample,
+      area: areas.sample,
+      tags:  tags.shuffle.take(3),
       icon: 'https://static.wixstatic.com/media/6c06b5_1ab8fe87a43143df915ee18213cdf864~mv2.jpg/v1/fill/w_512,h_404,al_c,q_80,usm_0.66_1.00_0.01/IMG_1435_JPG.webp'
     }
 
-    @tags = %w(おしゃれさん 電子音 シュッとしてる おじさん おさけ パワーポップ)
-    @areas = %w(高崎 大塚 うつのみや 東京)
+    @tags = Tag.all.map(&:name)
+    @areas = Area.all.map(&:name)
   end
 end
---
+++ lib/tasks/area.rake
@@ -0,0 +1,11 @@
+namespace :area do
+  desc 'Create records for initial state'
+  task create_initial_data: :environment do
+    names = %w(北海道 青森 岩手 宮城 秋田 山形 福島 茨城 栃木 宇都宮 足利 群馬 高崎 埼玉 越谷 千葉 東京 大塚 八王子 下北沢 吉祥寺 渋谷 新宿 神奈川 新潟 富山 石川 福井 山梨 長野 岐阜 静岡 愛知 三重 滋賀 京都 大阪 寝屋川 兵庫 姫路 奈良 和歌山 鳥取 島根 岡山 広島 山口 徳島 香川 愛媛 高知 福岡 佐賀 長崎 熊本 大分 宮崎 鹿児島 沖縄)
+    names.each do |name|
+      Area.create!(name: name)
+    rescue ActiveRecord::RecordInvalid
+      next
+    end
+  end
+end
